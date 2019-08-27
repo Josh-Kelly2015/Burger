818745925f8b16443b82188563d9b84ca25a183d
Joshua Kelly <jdkelly2015@gmail.com>
2019-08-27 09:41:53 -0400

    created sql db, tables, insert info. created connection to sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+  table_name (burger_name, devoured)
+VALUES
+  (baconator, 1);
+INSERT INTO
+  table_name (burger_name, devoured)
+VALUES(QuarterPounder, 0);
+INSERT INTO
+  table_name (burger_name, devoured)
+VALUES(Whopper, 1);
